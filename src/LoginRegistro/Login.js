6b68b13919233df59772092646838150af908ff3
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2022-01-30 14:19:35 -0500

    estilos de login y registro

--- src/LoginRegistro/Login.js
+++ src/LoginRegistro/Login.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Form from "react-bootstrap/Form";
+import "./LoginRegistro.scss";
 import { Link } from "react-router-dom";
-import "../index.css";
 import { url } from "../helpers/urlUsuarios";
-import { Button, Container, Stack } from "react-bootstrap";
+
 
 class Login extends Component {
   state = {
@@ -21,7 +20,7 @@ class Login extends Component {
         [e.target.name]: e.target.value,
       },
     });
-    //    console.log(this.state.form)
+       console.log(this.state.form)
   };
 
   iniciarSesion = async () => {
@@ -49,39 +48,49 @@ class Login extends Component {
 
   render() {
     return (
-      <div id="containerRegistro">
-        <Container id="containerLogin">
-          <Form>
-            <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
+      <div>
+        <img className="imagen" src='https://res.cloudinary.com/difxvqjj3/image/upload/v1643569386/Proyecto2/logo_m5bjlo.png' alt='logo' />
+       
+        <div className="containerLogin">
+        <div className="containerLogin">
+            <form className="formulario">
+            <div className="">
+              <label className="label">Email</label>
+              <input
+              className="email"
                 type="email"
                 name="correo"
                 onChange={this.handleChanged}
                 placeholder="Ingresa tu correo"
               />
-            </Form.Group>
+            </div>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
+            <div  className="">
+               <label className="label">Password</label>
+              <input
+              className="password"
                 type="password"
                 name="password"
                 onChange={this.handleChanged}
                 placeholder="Ingresa tu contraseña"
               />
-            </Form.Group>
+            </div>
 
-            <Stack gap={2} className="mb-3 mx-auto">
-              <Button variant="primary" onClick={() => this.iniciarSesion()}>
+            <div>
+              <button className="iniSesion" type="submit" onClick={() => this.iniciarSesion()}>
                 Iniciar sesión
-              </Button>
-              <Link className="btn btn-danger " to={`/Registro `}>
+              </button>
+
+              <button className="irRegistro">
+              <Link className="linkRegistro" to={`/Registro `}>
                 Registro
               </Link>
-            </Stack>
-          </Form>
-        </Container>
+              </button>
+             
+            </div>
+          </form>
+        </div>
+      </div>
       </div>
     );
   }
